66559743c5421a7d0c3719c04f36eba6c460b4c6
xxykz <fernandoarielrodriguez10@gmail.com>
2022-12-07 01:56:13 -0300

    getOperationsCountExample

--- examples/getOperationsCount.js
+++ examples/getOperationsCount.js
@@ -1,7 +1,7 @@
 const { ethers } = require("ethers");
 
 // API KEY DE INFURA
-const INFURA_ID = "INGRESA TU APIKEY O INFURA ID";
+const INFURA_ID = "6ad32fe8f2d64de4b501ab527cc49740";
 
 // UN PROVIDER ES LA PUERTA DE ENTRADA A LA LECTURA DE LA INFORMACION EN LA BLOCKCHAIN
 const provider = new ethers.providers.JsonRpcProvider(
@@ -12,11 +12,10 @@ const provider = new ethers.providers.JsonRpcProvider(
 const address = "0x0Ead685c9658E4cf1744d44305bc8d11c839F7db";
 
 // FUNCION ASYNCRONA QUE PREGUNTARA EL BALANCE DE LA ADDRESS PASADA A TRAVES DE NUESTRO PROVIDER (ACCESS TO READ THE BLOCKCHAIN)
-const getBalance = async () => {
- const balance = await provider.getBalance(address);
- // PARSEAMOS EL BIG NUMBER QUE NOS DEVUELVE EL FETCH
- console.log(`${address} tiene => `, ethers.utils.formatEther(balance));
+const getCountOperations = async () => {
+ const countOperations = await provider.getTransactionCount(address);
+ console.log(countOperations);
 };
 
 // EJECUTAMOS NUESTRO METODO
-getBalance();
+getCountOperations();
